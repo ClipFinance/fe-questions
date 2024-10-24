8a7b80fff3ab063b5f8b377ead60ff4bf406b545
Konstantin Kurganov <contact@hallstain.com>
2024-10-24 18:11:25 +0300

    Update 1.ts

--- 1.ts
+++ 1.ts
@@ -1,25 +1,25 @@
 const { data: singleCallData = BigInt(0) } = useReadContract({
   address,
-	abi,
-	functionName: 'balanceOf',
-	args: [address],
-	query: {
-		enabled: true
-	},
+  abi,
+  functionName: 'balanceOf',
+  args: [address],
+  query: {
+    enabled: true
+  },
 });
 
 
 const { data: multiCallData = [] } = useReadContracts({
-	contracts: contractsTemplates,
-	query: {
-		enabled: true
-	},
+  contracts: contractsTemplates,
+  query: {
+    enabled: true
+  },
 });
 
 const multicallDataAsObject = multicallData.reduce((acc, value, index) => {
-	return acc[index] = value
+  return acc[index] = value
 }, {})
 
 if (singleCallData && multicallDataAsObject) {
-	return singleCallData * multicallDataAsObject[0]
+  return singleCallData * multicallDataAsObject[0]
 }
